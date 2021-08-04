afc8875e52ae4c6a5b3966ed2b3313e8e3a15b0c
Joshua Curry <joshuacurry2615@gmail.com>
2021-08-04 16:02:38 -0400

    Set up skeleton for application

--- QuizApp/ContentView.swift
+++ QuizApp/ContentView.swift
@@ -2,15 +2,57 @@
 //  ContentView.swift
 //  QuizApp
 //
-//  Created by HBCU-c2-14 on 8/4/21.
+//  Created by Joshua Curry on 8/4/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    let question = "What was the first computer bug?"
+    let possibleAnswers = [
+        "Fly",
+        "Ant",
+        "Beetle",
+        "Moth"
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.sRGB, red: 0.7, green: 0.7, blue: 0.5, opacity: 0.2)
+                .ignoresSafeArea()
+            VStack {
+                Text("Quiz Time!")
+                    .font(.largeTitle)
+                    .padding()
+                Text("Question 1 / 4")
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Text(question)
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Spacer()
+                Spacer()
+                HStack{
+                    ForEach(possibleAnswers.indices){ index in
+                        AnswerButton(text: "\(possibleAnswers[index])")
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
+struct AnswerButton: View {
+    let text: String
+    var body: some View {
+        Button(action: {
+            print("You selected \(text)")
+        }) {
+            Text("\(text)")
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/4/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
