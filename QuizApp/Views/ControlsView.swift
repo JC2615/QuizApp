bfe68948be37e8151b6f6dd52b8b4845a12afd37
Joshua Curry <joshuacurry2615@gmail.com>
2021-08-11 16:18:05 -0400

    Added a shape picker to controls view

--- QuizApp/Views/ControlsView.swift
+++ QuizApp/Views/ControlsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ControlsView: View {
     @State var name = "user"
     @State var numOfShapes = 5
+    @State var shapeType = ShapeType.circle
     var body: some View {
         VStack {
             Text("Controls")
@@ -19,13 +20,34 @@ struct ControlsView: View {
             Spacer()
             HStack {
                 ForEach(0..<numOfShapes, id: \.self){ _ in
-                    Circle()
+                    switch shapeType {
+                        case .circle: Circle()
+                        case .capsule: Capsule()
+                        case .rectangle: Rectangle()
+                    }
                 }
             }
             Form {
                 TextField("Johnny Appleseed", text: $name)
                 Stepper("Number of shapes \(numOfShapes)", value: $numOfShapes, in: 1...8)
+                Picker("Shape Picker", selection: $shapeType, content: {
+                    ForEach(ShapeType.allCases) { shape in
+                        Text(shape.rawValue.capitalized)
+                            .tag(shape)
+                    }
+                })
+                .pickerStyle(SegmentedPickerStyle())
             }
+        }
+    }
+    
+    enum ShapeType: String, CaseIterable, Identifiable {
+        case circle
+        case rectangle
+        case capsule
+        
+        var id: String {
+            self.rawValue
         }
     }
 }
